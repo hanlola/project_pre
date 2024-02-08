6e96fcc1f291ce09a5ec8537eecf3153cc6284b6
hanlola <157224646+hanlola@users.noreply.github.com>
2024-02-09 01:06:18 +0900

    Add files via upload

---
+++ project_prerequisite/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        // implement algorithm here
+        //  new UserDaoJDBCImpl().createUsersTable();
+//        new UserDaoJDBCImpl().saveUser("Lola","Han",(byte) 27);
+//        System.out.println("Пользователь с именем Лола добавлен в базу данных");
+//        new UserDaoJDBCImpl().saveUser("Vlad", "Tsoy", (byte)28);
+//        System.out.println("Пользователь с именем VLAD добавлен в базу данных");
+//        new UserDaoJDBCImpl().saveUser("Tema","Tsoy",(byte) 1);
+//        System.out.println("Пользователь с именем Tema добавлен в базу данных");
+//        new UserDaoJDBCImpl().saveUser("Artur","Han",(byte) 20);
+//        System.out.println("Пользователь с именем Artur добавлен в базу данных");
+//        System.out.println(new UserDaoJDBCImpl().getAllUsers());
+//        new UserDaoJDBCImpl().cleanUsersTable();
+//        new UserDaoJDBCImpl().dropUsersTable();
+        new UserServiceImpl().createUsersTable();
+        new UserServiceImpl().saveUser("Lola","Han",(byte) 27);
+        System.out.println("Пользователь с именем - Lola добавлен в базу данных");
+        new UserServiceImpl().saveUser("Vlad", "Tsoy", (byte)28);
+        System.out.println("Пользователь с именем - Vlad добавлен в базу данных");
+        new UserServiceImpl().saveUser("Tema","Tsoy",(byte) 1);
+        System.out.println("Пользователь с именем - Tema добавлен в базу данных");
+        new UserServiceImpl().saveUser("Artur","Han",(byte) 20);
+        System.out.println("Пользователь с именем - Artur добавлен в базу данных");
+        System.out.println(new UserServiceImpl().getAllUsers());
+        new UserServiceImpl().cleanUsersTable();
+        new UserServiceImpl().dropUsersTable();
+    }
+}
