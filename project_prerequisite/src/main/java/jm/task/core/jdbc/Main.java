ba0e7dcc507c0fa432119fe8b8bc55e135269e1e
hanlola <157224646+hanlola@users.noreply.github.com>
2024-02-14 00:58:26 +0900

    Update Main.java

--- project_prerequisite/src/main/java/jm/task/core/jdbc/Main.java
+++ project_prerequisite/src/main/java/jm/task/core/jdbc/Main.java
@@ -9,13 +9,12 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        // implement algorithm here
-          new UserDaoJDBCImpl().createUsersTable();
+        new UserDaoJDBCImpl().createUsersTable();
         new UserDaoJDBCImpl().saveUser("Lola","Han",(byte) 27);
         new UserDaoJDBCImpl().saveUser("Vlad", "Tsoy", (byte)28);
         new UserDaoJDBCImpl().saveUser("Tema","Tsoy",(byte) 1);
         new UserDaoJDBCImpl().saveUser("Artur","Han",(byte) 20);
-        new UserDaoJDBCImpl().getAllUsers();
+        System.out.println(new UserDaoJDBCImpl().getAllUsers());
         new UserDaoJDBCImpl().cleanUsersTable();
         new UserDaoJDBCImpl().dropUsersTable();
 
